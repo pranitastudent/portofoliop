8d17d70168dff2c45427144b43ac315c23755899
pranitastudent <pranita_gandhi@hotmail.com>
2019-12-08 12:51:36 +0000

    added4

--- porty/settings.py
+++ porty/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'djangodbman',
-        'PASSWORD':'Maturase13',
+        'PASSWORD':'Maturase14!',
         'HOST': 'localhost',
         'PORT': '5432',
     }
